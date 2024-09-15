02e1a414908a12f6f9ad77aa82e4c06de0d92b1d
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-09-15 15:37:21 -0400

    Add ALongTimeAgoView

--- StarWarsScroller/Sources/StarWarsScroller/ALongTimeAgoView.swift
+++ StarWarsScroller/Sources/StarWarsScroller/ALongTimeAgoView.swift
@@ -1,5 +1,5 @@
 //
-//  SwiftUIView.swift
+//  ALongTimeAgoView.swift
 //  
 //
 //  Created by Jeffrey Sweeney on 9/15/24.
@@ -7,12 +7,27 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct ALongTimeAgoView: View {
+    @State private var showingFirstText = false
+    @State private var showingSecondText = false
+    
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 32) {
+            Text("A long time ago in a galaxy far, far away ....")
+                .modifier(CWIntroEaseInModifier(introMessage: .firstText, showingContent: $showingFirstText))
+            
+            Text("Or probably just sometime in 2024 from my home in Maryland ...")
+                .modifier(CWIntroEaseInModifier(introMessage: .secondText, showingContent: $showingSecondText))
+        }
+        .font(.title)
+        .foregroundStyle(.blue)
+        .fontWeight(.semibold)
+        .padding(.horizontal)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
     }
 }
 
 #Preview {
-    SwiftUIView()
+    ALongTimeAgoView()
 }
